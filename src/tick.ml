6b4acbe2ccab04ae230bfb793c92a434d979f4c3
Vincent Bernardoff <vb@luminar.eu.org>
2017-04-29 16:49:31 +0200

    Initial commit.

---
+++ src/tick.ml
@@ -0,0 +1,332 @@
+open Core
+
+type side = [`Buy | `Sell]
+
+type t = {
+  ts: Time_ns.t;
+  side: [`Buy | `Sell] option;
+  p: Int63.t;
+  v: Int63.t;
+}
+
+let create ?side ~ts ~p ~v () = { ts ; side ; p ; v }
+
+let side_of_int64 = function
+  | 0L -> None
+  | 1L -> Some `Buy
+  | 2L -> Some `Sell
+  | _ -> invalid_arg "side_of_int64"
+
+let int64_of_side = function
+  | None -> 0L
+  | Some `Buy -> 1L
+  | Some `Sell -> 2L
+
+let int64_of_v_side v side =
+  let open Int64 in
+  let side_i = int64_of_side side in
+  let v = Int63.to_int64 v in
+  bit_or (bit_and v (shift_left one 62 - one)) (shift_left side_i 62)
+
+let v_side_of_int64 i =
+  try
+    let open Int64 in
+    let side = side_of_int64 @@ shift_right_logical i 62 in
+    let v = bit_and i (shift_left 1L 62 - 1L) in
+    Int63.of_int64_exn v, side
+  with _ -> invalid_arg @@ Printf.sprintf "%Lx\n%!" i
+
+module type IO = sig
+  type t
+  val length : t -> int
+  val get_int64_be : t -> int -> int64
+  val set_int64_be : t -> int -> int64 -> unit
+  val get_int64_le : t -> int -> int64
+  val set_int64_le : t -> int -> int64 -> unit
+end
+
+module TickIO (IO: IO) = struct
+  open IO
+
+  let write ?(key_pos=0) ?(data_pos=0) ~buf_key ~buf_data { p; v; side; ts } =
+    set_int64_be buf_key key_pos @@ Int63.to_int64 @@ Time_ns.to_int63_ns_since_epoch ts;
+    set_int64_le buf_data data_pos @@ Int63.to_int64 p;
+    set_int64_le buf_data (data_pos+8) @@ int64_of_v_side v side
+
+  let read ?(pos=0) b =
+    let ts = get_int64_be b pos |> Int63.of_int64_exn |> Time_ns.of_int63_ns_since_epoch in
+    let p = Int63.of_int64_exn @@ get_int64_le b (pos+8) in
+    let v = get_int64_le b (pos+16) in
+    let v, side = v_side_of_int64 v in
+    create ~ts ~p ~v ?side ()
+
+  let read' ?(pos=0) ~ts ~data () =
+    let p = Int63.of_int64_exn @@ get_int64_le data pos in
+    let v = get_int64_le data (pos+8) in
+    let v, side = v_side_of_int64 v in
+    create ~ts ~p ~v ?side ()
+end
+
+module BytesIO = struct
+  include String
+  let get_int64_be buf pos = Binary_packing.unpack_signed_64_big_endian ~buf ~pos
+  let set_int64_be buf pos = Binary_packing.pack_signed_64_big_endian ~buf ~pos
+  let get_int64_le buf pos = Binary_packing.unpack_signed_64_little_endian ~buf ~pos
+  let set_int64_le buf pos = Binary_packing.pack_signed_64_little_endian ~buf ~pos
+end
+
+module BigstringIO = struct
+  include Bigstring
+  let get_int64_be buf pos = unsafe_get_int64_t_be buf ~pos
+  let set_int64_be buf pos = unsafe_set_int64_t_be buf ~pos
+  let get_int64_le buf pos = unsafe_get_int64_t_le buf ~pos
+  let set_int64_le buf pos = unsafe_set_int64_t_le buf ~pos
+end
+
+module B = Bytes
+
+module Bytes = TickIO(BytesIO)
+module Bigstring = TickIO(BigstringIO)
+
+let hdr_size = 16
+let size = 24
+let version = 1
+
+let hdr =
+  let b = String.create 16 in
+  String.blit "TICK" 0 b 0 4;
+  Binary_packing.pack_unsigned_16_little_endian b 4 hdr_size;
+  Binary_packing.pack_unsigned_16_little_endian b 6 size;
+  Binary_packing.pack_unsigned_16_little_endian b 8 version;
+  b
+
+let of_scid_record r =
+  let ts = (r.Scid.R.datetime -. 25569.) *.  86400. *. 1e9 in (* unix ts in nanoseconds *)
+  create
+    ~ts:(ts |> Int63.of_float |> Time_ns.of_int63_ns_since_epoch)
+    ~p:(Int63.of_float @@ r.Scid.R.c *. 1e8)
+    ~v:(Int63.(of_int64_exn r.Scid.R.total_volume * of_int 10_000))
+    ~side:(if r.Scid.R.bid_volume = 0L then `Buy else `Sell) ()
+
+let key = B.create 8
+let data = B.create 16
+
+module type LDB = sig
+  exception Error of string
+  type db
+  type iterator
+  type writebatch
+  type comparator
+  type env
+  val destroy : string -> bool
+  val repair : string -> bool
+  val default_env : env
+  val lexicographic_comparator : comparator
+  val open_db :
+    ?write_buffer_size:int ->
+    ?max_open_files:int ->
+    ?block_size:int ->
+    ?block_restart_interval:int ->
+    ?comparator:comparator -> ?cache_size:int -> ?env:env -> string -> db
+  val close : db -> unit
+  val get : db -> string -> string option
+  val get_exn : db -> string -> string
+  val mem : db -> string -> bool
+  val put : db -> ?sync:bool -> string -> string -> unit
+  val delete : db -> ?sync:bool -> string -> unit
+  val iter : (string -> string -> bool) -> db -> unit
+  val rev_iter : (string -> string -> bool) -> db -> unit
+  val iter_from : (string -> string -> bool) -> db -> string -> unit
+  val rev_iter_from : (string -> string -> bool) -> db -> string -> unit
+  module Batch : sig
+    val make : unit -> writebatch
+    val put : writebatch -> string -> string -> unit
+    val put_substring :
+      writebatch -> string -> int -> int -> string -> int -> int -> unit
+    val delete : writebatch -> string -> unit
+    val delete_substring : writebatch -> string -> int -> int -> unit
+    val write : db -> ?sync:bool -> writebatch -> unit
+  end
+  module Iterator : sig
+    val make : ?fill_cache:bool -> db -> iterator
+    val close : iterator -> unit
+    val seek_to_first : iterator -> unit
+    val seek_to_last : iterator -> unit
+    val seek: iterator -> string -> int -> int -> unit
+    val next : iterator -> unit
+    val prev : iterator -> unit
+    val valid : iterator -> bool
+    val fill_key : iterator -> string ref -> int
+    val fill_value : iterator -> string ref -> int
+    val get_key : iterator -> string
+    val get_value : iterator -> string
+    val iter : (string -> string -> bool) -> iterator -> unit
+    val rev_iter : (string -> string -> bool) -> iterator -> unit
+    val iter_from : (string -> string -> bool) -> iterator -> string -> unit
+    val rev_iter_from : (string -> string -> bool) -> iterator -> string -> unit
+  end
+end
+
+module type LDB_WITH_TICK = sig
+  include LDB
+
+  val length : db -> int
+  val bounds : db -> (t * t) option
+  val mem_tick : db -> t -> bool
+  val put_tick : ?sync:bool -> db -> t -> unit
+  val put_tick_batch : writebatch -> t -> unit
+  val put_ticks : ?sync:bool -> db -> t list -> unit
+  val get_tick : db -> int64 -> t option
+end
+
+module MakeLDB(DB : LDB) = struct
+  include DB
+
+  let length db =
+    let cnt = ref 0 in
+    iter (fun _ _ -> incr cnt; true) db;
+    !cnt
+
+  let bounds db =
+    if length db = 0 then None
+    else
+      let min_elt = String.create size in
+      let max_elt = String.create size in
+      iter (fun k v ->
+          String.blit k 0 min_elt 0 8;
+          String.blit v 0 min_elt 8 16;
+          false) db;
+      rev_iter (fun k v ->
+          String.blit k 0 max_elt 0 8;
+          String.blit v 0 min_elt 8 16;
+          false) db;
+      Some (Bytes.read min_elt,
+            Bytes.read max_elt)
+
+  let mem_tick db t =
+    Binary_packing.pack_signed_64_big_endian key 0 (Time_ns.to_int63_ns_since_epoch t.ts |> Int63.to_int64);
+    mem db key
+
+  let put_tick ?sync db t =
+    Bytes.write ~buf_key:key ~buf_data:data t;
+    put ?sync db key data
+
+  let put_tick_batch batch t =
+    Bytes.write ~buf_key:key ~buf_data:data t;
+    Batch.put batch key data
+
+  let put_ticks ?sync db ts =
+    let batch = Batch.make () in
+    List.iter ts ~f:begin fun t ->
+      Bytes.write ~buf_key:key ~buf_data:data t;
+      Batch.put batch key data
+    end;
+    Batch.write ?sync db batch
+
+  let get_tick db ts =
+    let open Option.Monad_infix in
+    Binary_packing.pack_signed_64_big_endian key 0 ts;
+    get db key >>| fun data ->
+    let ts = Int63.of_int64_exn ts |> Time_ns.of_int63_ns_since_epoch in
+    Bytes.read' ~ts ~data ()
+end
+
+module File = struct
+  let of_scid ic oc =
+    let open Scid in
+    let buf = B.create size in
+    let d = D.make @@ Channel ic in
+    let rec loop nb_records = match D.decode d with
+      | R r ->
+        let t = of_scid_record r in
+        Bytes.write ~buf_key:key ~buf_data:data t;
+        Out_channel.output oc buf 0 size;
+        loop (succ nb_records)
+      | End -> nb_records
+      | Error e -> failwith (Format.asprintf "%a" D.pp_error e)
+      | Await -> failwith "Await"
+    in
+    Out_channel.output_string oc hdr;
+    loop 0
+
+  let bounds ic =
+    let buf = B.create size in
+    let nb_read = In_channel.input ic ~buf ~pos:0 ~len:hdr_size in
+    if nb_read <> hdr_size || String.sub buf 0 hdr_size <> hdr
+    then Error (Invalid_argument
+                  (Printf.sprintf "corrupted header: read %S"
+                     (String.sub buf 0 nb_read)))
+    else
+      let nb_read_fst = In_channel.input ic ~buf ~pos:0 ~len:size in
+      let min_elt = Bytes.read buf in
+      In_channel.(Int64.(seek ic @@ length ic - of_int size));
+      let nb_read_snd = In_channel.input ic ~buf ~pos:0 ~len:size in
+      let max_elt = Bytes.read buf in
+      if nb_read_fst + nb_read_snd <> 2 * size
+      then Error End_of_file
+      else (In_channel.seek ic 0L; Result.return (min_elt, max_elt))
+
+  let of_db
+      (type db)
+      (ldb : (module LDB_WITH_TICK with type db = db))
+      ?start_ts ?end_ts (db:db) oc =
+    let module DB = (val ldb : LDB_WITH_TICK with type db = db) in
+    let open Result in
+    Out_channel.output_string oc hdr;
+    let cnt = ref 0 in
+    Result.of_option
+      ~error:(Failure "bounds") (DB.bounds db) >>= fun (min_elt, max_elt) ->
+    let nb_ticks = ref 0 in
+    DB.iter (fun k v ->
+        incr nb_ticks;
+        Out_channel.output oc ~buf:k ~pos:0 ~len:8;
+        Out_channel.output oc ~buf:v ~pos:0 ~len:16;
+        true) db;
+    return
+      (if (!cnt = 2) then !nb_ticks, Some (min_elt, max_elt) else 0, None)
+
+  let to_db
+    (type db)
+    (ldb : (module LDB_WITH_TICK with type db = db))
+    ic (db:db) =
+    let module DB = (val ldb : LDB_WITH_TICK with type db = db) in
+    let open Result in
+    bounds ic >>= fun (min_elt, max_elt) ->
+    let buf = B.create size in
+    let cnt = Int64.((In_channel.length ic - of_int hdr_size) / (of_int size) |> to_int_exn) in
+    if cnt = 0 then
+      Ok (0, None)
+    else begin
+      In_channel.seek ic 16L;
+      let rec loop () =
+        In_channel.really_input ic ~buf ~pos:0 ~len:size |> function
+        | None -> Ok (cnt, Some (min_elt, max_elt))
+        | Some () -> DB.put db (String.sub buf 0 8) (String.sub buf 8 16); loop ()
+      in loop ()
+    end
+
+  let db_of_scid
+    (type db)
+    (ldb : (module LDB_WITH_TICK with type db = db))
+    ?(offset=Time_ns.epoch) db fn =
+    let module DB = (val ldb : LDB_WITH_TICK with type db = db) in
+    let open Scid in
+    let buf = B.create size in
+    In_channel.with_file ~binary:true fn ~f:(fun ic ->
+        let d = D.make @@ Channel ic in
+        let rec loop nb_records = match D.decode d with
+          | R r ->
+            let t = of_scid_record r in
+            if Time_ns.(t.ts > offset) then begin
+              Bytes.write ~buf_key:key ~buf_data:data t;
+              DB.put db (String.sub buf 0 8) (String.sub buf 8 16);
+              loop @@ succ nb_records
+            end
+            else loop nb_records
+          | End -> nb_records
+          | Error e -> failwith (Format.asprintf "%a" D.pp_error e)
+          | Await -> failwith "Await"
+        in
+        loop 0
+      )
+end
---
+++ src/tick.mli
@@ -0,0 +1,132 @@
+open Core
+
+type side = [`Buy | `Sell]
+
+type t = {
+  ts: Time_ns.t;
+  side: side option;
+  p: Int63.t;
+  v: Int63.t;
+}
+
+val create : ?side:side -> ts:Time_ns.t -> p:Int63.t -> v:Int63.t -> unit -> t
+
+module Bytes : sig
+  val write : ?key_pos:int -> ?data_pos:int -> buf_key:Bytes.t -> buf_data:Bytes.t -> t -> unit
+  val read : ?pos:int -> Bytes.t -> t
+  val read' : ?pos:int -> ts:Time_ns.t -> data:Bytes.t -> unit -> t
+end
+
+module Bigstring : sig
+  val write : ?key_pos:int -> ?data_pos:int -> buf_key:Bigstring.t -> buf_data:Bigstring.t -> t -> unit
+  val read : ?pos:int -> Bigstring.t -> t
+  val read' : ?pos:int -> ts:Time_ns.t -> data:Bigstring.t -> unit -> t
+end
+
+val hdr : string
+val hdr_size : int
+val size : int
+val version : int
+
+val of_scid_record : Scid.R.t -> t
+
+module type LDB = sig
+  exception Error of string
+  type db
+  type iterator
+  type writebatch
+  type comparator
+  type env
+  val destroy : string -> bool
+  val repair : string -> bool
+  val default_env : env
+  val lexicographic_comparator : comparator
+  val open_db :
+    ?write_buffer_size:int ->
+    ?max_open_files:int ->
+    ?block_size:int ->
+    ?block_restart_interval:int ->
+    ?comparator:comparator -> ?cache_size:int -> ?env:env -> string -> db
+  val close : db -> unit
+  val get : db -> string -> string option
+  val get_exn : db -> string -> string
+  val mem : db -> string -> bool
+  val put : db -> ?sync:bool -> string -> string -> unit
+  val delete : db -> ?sync:bool -> string -> unit
+  val iter : (string -> string -> bool) -> db -> unit
+  val rev_iter : (string -> string -> bool) -> db -> unit
+  val iter_from : (string -> string -> bool) -> db -> string -> unit
+  val rev_iter_from : (string -> string -> bool) -> db -> string -> unit
+  module Batch :
+  sig
+    val make : unit -> writebatch
+    val put : writebatch -> string -> string -> unit
+    val put_substring :
+      writebatch -> string -> int -> int -> string -> int -> int -> unit
+    val delete : writebatch -> string -> unit
+    val delete_substring : writebatch -> string -> int -> int -> unit
+    val write : db -> ?sync:bool -> writebatch -> unit
+  end
+  module Iterator : sig
+    val make : ?fill_cache:bool -> db -> iterator
+    val close : iterator -> unit
+    val seek_to_first : iterator -> unit
+    val seek_to_last : iterator -> unit
+    val seek: iterator -> string -> int -> int -> unit
+    val next : iterator -> unit
+    val prev : iterator -> unit
+    val valid : iterator -> bool
+    val fill_key : iterator -> string ref -> int
+    val fill_value : iterator -> string ref -> int
+    val get_key : iterator -> string
+    val get_value : iterator -> string
+    val iter : (string -> string -> bool) -> iterator -> unit
+    val rev_iter : (string -> string -> bool) -> iterator -> unit
+    val iter_from : (string -> string -> bool) -> iterator -> string -> unit
+    val rev_iter_from : (string -> string -> bool) -> iterator -> string -> unit
+  end
+end
+
+module type LDB_WITH_TICK = sig
+  include LDB
+
+  val length : db -> int
+  val bounds : db -> (t * t) option
+  val mem_tick : db -> t -> bool
+  val put_tick : ?sync:bool -> db -> t -> unit
+  val put_tick_batch : writebatch -> t -> unit
+  val put_ticks : ?sync:bool -> db -> t list -> unit
+  val get_tick : db -> int64 -> t option
+end
+
+module MakeLDB(DB : LDB) : LDB_WITH_TICK with type db = DB.db
+
+module File : sig
+  (** Functions that operatate on tick files. *)
+
+  (** Convenience functions *)
+
+  val bounds : In_channel.t -> (t * t, exn) Result.t
+  (** [bounds ic] is the first and the last tick found in [ic], if
+      any. If there is no error, [In_channel.t] will be positioned at
+      the beginning of the file. *)
+
+  (** Conversion from/to the SCID file format. *)
+
+  val of_scid : In_channel.t -> Out_channel.t -> int
+
+  val db_of_scid :
+    (module LDB_WITH_TICK with type db = 'a) ->
+    ?offset:Time_ns.t -> 'a -> string -> int
+
+  (** Conversion of tick files from and to LevelDB. *)
+
+  val to_db :
+    (module LDB_WITH_TICK with type db = 'a) ->
+    In_channel.t -> 'a -> (int * (t * t) option, exn) Result.t
+
+  val of_db :
+    (module LDB_WITH_TICK with type db = 'a) ->
+    ?start_ts:int64 -> ?end_ts:int64 -> 'a ->
+    Out_channel.t -> (int * (t * t) option, exn) Result.t
+end
